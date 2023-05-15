cb1ab1ca84f86cf35f02eaa26009900d8e8bee96
KoshakiDev <xanthosgardentstudios@gmail.com>
2023-05-15 19:42:09 +0200

    adding laser gun sprites

---
+++ Physics/Components/FadingSprite.cs
@@ -0,0 +1,41 @@
+﻿using GXPEngine;
+using System;
+
+[Serializable]
+public class FadingSprite : Component
+{
+
+    float fadeTime = 20f;
+
+    float currentFadeTime = 0;
+
+    public FadingSprite(GameObject owner, params string[] args) : base(owner)
+    {
+
+    }
+
+    public override void Refresh()
+    {
+        base.Refresh();
+    }
+    protected override void Update()
+    {
+        base.Update();
+        Fade();
+    }
+
+    public void Fade()
+    {
+        if (currentFadeTime > fadeTime)
+        {
+            return;
+        }
+        
+        currentFadeTime += 1.0f / (float) Time.deltaTime;
+        Debug.Log("time " + currentFadeTime);
+
+        Sprite sprite = (Sprite)Owner;
+        sprite.color = (uint)sprite.InterpolateColor(0xFFFFFF, 0x000000, currentFadeTime / fadeTime);
+    }
+
+}
